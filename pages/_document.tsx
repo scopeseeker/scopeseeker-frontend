ef0bf0f9e97bdfe88b56cc80933a7d2594dca022
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-03-17 20:36:51 +0530

    rfc: default code for index and document page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import theme from '../styles/theme'
-import { ColorModeScript } from '@chakra-ui/react'
+import theme from '../styles/theme';
+import { ColorModeScript } from '@chakra-ui/react';
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Scope Seeker</title>
+        <meta name="description" content="Scope Seeker is a cutting-edge web application designed to help job seekers find their dream job with ease." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
