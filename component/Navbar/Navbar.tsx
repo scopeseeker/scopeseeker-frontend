41cdf9daba0c9bc2974bee8822d334369390ad05
Abhishek <abhishekkumar0336165@gmail.com>
2023-03-20 21:48:47 +0530

    style: navbar update

--- component/Navbar/Navbar.tsx
+++ component/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Button, HStack, Text, VStack } from '@chakra-ui/react';
-import Link from 'next/link';
+import Image from 'next/image';
+import Icon from '../Icon/Icon';
 
 const Navbar = () => {
   return (
@@ -12,18 +13,48 @@ const Navbar = () => {
         borderBottomEndRadius={'2xl'}
         borderTopLeftRadius={'2xl'}
       >
-        <Text as={'span'} fontWeight={'hairline'} fontSize={'sm'}>
-          Scope Seeker
-        </Text>
-
-        <HStack fontSize={'xs'} gap={2.5}>
-          <Link href={'#'}>Jobs</Link>
-          <Link href={'#'}>Company</Link>
-          <Link href={'#'}>About Us</Link>
+        <HStack>
+          <Image
+            src={require('../../public/scopeseeker-logo.png')}
+            alt={'navbar'}
+            height={28}
+          />
+          <Text as={'span'} fontWeight={'hairline'} fontSize={'sm'}>
+            Scope Seeker
+          </Text>
         </HStack>
 
-        <HStack fontSize={'xs'} gap={2.5}>
-          {/* <Link href={'#'}>Login</Link> */}
+        <HStack spacing={4}>
+          <Button
+            variant={'link'}
+            color={'blackAlpha.800'}
+            fontSize={'xs'}
+            fontWeight={'medium'} 
+            leftIcon={<Icon name="job" width="14px" height="14px" />}
+          >
+            Job
+          </Button>
+          <Button
+            variant={'link'}
+            color={'blackAlpha.800'}
+            fontSize={'xs'}
+            fontWeight={'medium'} 
+            leftIcon={<Icon name="company" width="14px" height="14px"/>}
+          >
+            Company
+          </Button>
+          <Button
+            variant={'link'}
+            color={'blackAlpha.800'}
+            fontSize={'xs'}
+            fontWeight={'medium'} 
+            leftIcon={<Icon name="pages" width="14px" height="14px" />}
+          >
+            Page
+          </Button>
+        </HStack>
+
+        <HStack fontSize={'xs'}>
           <Button
             colorScheme={'none'}
             size={'sm'}
@@ -38,9 +69,16 @@ const Navbar = () => {
             size={'sm'}
             fontSize={'xs'}
             fontWeight={'thin'}
+            color={'white'}
           >
             Sign Up
           </Button>
+          <Icon
+            name="darkMode"
+            width="18px"
+            height="18px"
+            style={{ cursor: 'pointer', marginLeft: '15px' }}
+          />
         </HStack>
       </HStack>
     </VStack>
