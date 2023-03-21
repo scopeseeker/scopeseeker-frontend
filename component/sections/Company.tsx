a4b09de838c83b930c6f92b1771c6f497aca4d02
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-03-21 21:41:05 +0530

    style: curve fotter section added

---
+++ component/sections/Company.tsx
@@ -0,0 +1,12 @@
+import { HStack } from '@chakra-ui/react'
+import React from 'react'
+
+const Company = () => {
+  return (
+    <HStack width={'95%'} borderRadius={10} h={40} bg={'red'} mx={'auto'}>
+
+    </HStack>
+  )
+}
+
+export default Company
