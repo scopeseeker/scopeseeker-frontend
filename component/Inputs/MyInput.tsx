014b8a677fb38f364691c4b5a6fe13f2c716ad95
Abhishek <abhishekkumar0336165@gmail.com>
2023-04-01 12:02:53 +0530

    fix: deploy bug fixed try

--- component/Inputs/MyInput.tsx
+++ component/Inputs/MyInput.tsx
@@ -1,39 +1,43 @@
 import { FormLabel, Input } from '@chakra-ui/react';
 
 interface IMyInputProps {
-    inputFontSize?: string;
-    labelFontSise?: string;
-    labelTitle?: string;
-    placeholder?: string;
-    size?: string;
-    type: string;
+  inputFontSize?: string;
+  labelFontSise?: string;
+  labelTitle?: string;
+  placeholder?: string;
+  size?: string;
+  type: string;
+  mt?: number;
 }
 
 const MyInput: React.FC<IMyInputProps> = (props) => {
-    const {
-        inputFontSize = 'sm',
-        labelFontSise = 'xs',
-        labelTitle,
-        placeholder,
-        type,
-        size,
-    } = props
-    return (
-        <>
-            <FormLabel fontSize={labelFontSise}>{labelTitle}</FormLabel>
-            <Input
-                colorScheme={'messenger'}
-                type={type}
-                placeholder={placeholder}
-                fontSize={inputFontSize}
-                size={size}
-                variant={'filled'}
-                bg={'brand.aliceblue'}
-                _hover={{ backgroundColor: 'brand.aliceblue' }}
-                _placeholder={{ color: 'brand.darkgrey' }}
-            />
-        </>
-    );
+  const {
+    inputFontSize = 'sm',
+    labelFontSise = 'xs',
+    labelTitle,
+    placeholder,
+    type,
+    size,
+    mt,
+  } = props;
+  return (
+    <>
+      <FormLabel mt={mt} fontSize={labelFontSise}>
+        {labelTitle}
+      </FormLabel>
+      <Input
+        colorScheme={'messenger'}
+        type={type}
+        placeholder={placeholder}
+        fontSize={inputFontSize}
+        size={size}
+        variant={'filled'}
+        bg={'brand.aliceblue'}
+        _hover={{ backgroundColor: 'brand.aliceblue' }}
+        _placeholder={{ color: 'brand.darkgrey', fontSize: inputFontSize }}
+      />
+    </>
+  );
 };
 
 export default MyInput;
