7c08ff052177c5ec42bbfaf2bdad2c627e593b3f
Abhijeet Raj <abhijeetraj900@gmail.com>
2023-04-15 18:47:55 +0530

    rfc: code added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import Footer from '@/component/footer';
 import Navbar from '@/component/navbar/Navbar';
+import '@/styles/globals.css';
 import theme from '@/styles/theme';
 import { ChakraProvider, Flex } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
-import NewsUpdate from '@/component/sections/NewsUpdate';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -17,9 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       {showNavbar && <Navbar />}
-      <Flex flexDirection={'column'} gap={24}>
+      <Flex flexDirection={'column'}>
         <Component {...pageProps} />
-        {showNavbar && <NewsUpdate />}
         {showNavbar && <Footer />}
       </Flex>
     </ChakraProvider>
