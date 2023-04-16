66f1b41c7871bcc4573347165a22d781a1d2d8a5
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2023-04-16 17:06:29 +0530

    style: about us updated

--- component/heading/MyHeading.tsx
+++ component/heading/MyHeading.tsx
@@ -7,13 +7,21 @@ interface MyHeadingProps {
   align?: string;
   headColor?: string;
   paraColor?: string;
+  titleFontSize?: string;
 }
 
 const MyHeading: React.FC<MyHeadingProps> = (props) => {
-  const { title, subTitle, align = 'center', headColor = 'brand.semiblack', paraColor = 'brand.darkgrey' } = props;
+  const {
+    title,
+    subTitle,
+    align = 'center',
+    headColor = 'brand.semiblack',
+    paraColor = 'brand.darkgrey',
+    titleFontSize,
+  } = props;
   return (
     <VStack alignItems={align}>
-      <Heading color={headColor}>
+      <Heading color={headColor} fontSize={titleFontSize}>
         {title}
       </Heading>
       <Text color={paraColor}>{subTitle}</Text>
--- component/icon/Icon.tsx
+++ component/icon/Icon.tsx
@@ -1,56 +1,60 @@
-import { IconLibrary } from "@/constant/IconLibrary";
-import React from "react";
+import { IconLibrary } from '@/constant/IconLibrary';
+import React from 'react';
 
 export type IconNameType =
-	| "job"
-	| "instagram"
-	| "darkMode"
-	| "company"
-	| "pages"
-	| "arrow-down"
-	| "search"
-	| "linkedin"
-	| "gmail"
-	| "facebook"
-	| "arrow"
-	| 'whatsapp'
-	| "twitter"
-	| "quotes-left"
-	| "quotes-right"
-	| "user"
-	| "case"
-	| "form"
-	| "phone"
-	| "fresher"
-	| "global"
-	| "college"
-	| "add"
-	| "github"
-	| "check"
-	| "searchicon"
-	| "save"
-	| "share"
-	| "location"
-	;
+  | 'job'
+  | 'instagram'
+  | 'darkMode'
+  | 'company'
+  | 'pages'
+  | 'arrow-down'
+  | 'search'
+  | 'linkedin'
+  | 'gmail'
+  | 'facebook'
+  | 'arrow'
+  | 'whatsapp'
+  | 'twitter'
+  | 'quotes-left'
+  | 'quotes-right'
+  | 'user'
+  | 'case'
+  | 'form'
+  | 'phone'
+  | 'fresher'
+  | 'global'
+  | 'college'
+  | 'add'
+  | 'github'
+  | 'check'
+  | 'searchicon'
+  | 'save'
+  | 'share'
+  | 'location'
+  | 'time';
 export type IconProps = {
-	name: IconNameType;
-	color?: string;
-	width?: string;
-	height?: string;
-	style?: React.CSSProperties;
+  name: IconNameType;
+  color?: string;
+  width?: string;
+  height?: string;
+  style?: React.CSSProperties;
 };
 
-
 const Icon = ({ name, style, ...rest }: IconProps) => {
-	const transform = IconLibrary[name]?.transform;
-	const path = IconLibrary[name]?.path;
+  const transform = IconLibrary[name]?.transform;
+  const path = IconLibrary[name]?.path;
 
-	return (
-		<svg xmlns="http://www.w3.org/2000/svg" style={style} viewBox="0 0 24 24" {...rest} className="custom-icon">
-			<path d={path} fill="currentColor" transform={transform} />
-		</svg>
-	);
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      style={style}
+      viewBox="0 0 24 24"
+      {...rest}
+      className="custom-icon"
+    >
+      <path d={path} fill="currentColor" transform={transform} />
+    </svg>
+  );
 };
 
 export default Icon;
-
