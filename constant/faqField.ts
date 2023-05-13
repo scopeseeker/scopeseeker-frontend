e2ab246ae452520e79db9ba348a7e208f2e60995
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-05-13 17:48:51 +0530

    refactor: refactor faq by applying map function from constant faqobjectfiled

---
+++ constant/faqField.ts
@@ -0,0 +1,25 @@
+const faqFieldsObject = [
+  {
+    qus: 'How does Scope Seeker verify job openings?',
+    ans: 'We verify job openings by directly sourcing them from reputable company career pages.',
+  },
+  {
+    qus: 'How does personalized job recommendation work?',
+    ans: 'Our algorithm analyzes your profile to recommend relevant job listings based on your skills and preferences.',
+  },
+  {
+    qus: 'Can I set up customized job alerts?',
+    ans: 'Yes, you can set personalized job alerts based on your preferred criteria and receive email notifications for new listings.',
+  },
+  {
+    qus: 'Is Scope Seeker available on mobile devices?',
+    ans: 'Yes, Scope Seeker is designed for optimal use on both mobile and desktop devices.',
+  },
+  {
+    qus: 'How does Scope Seeker protect user information?',
+    ans: 'We prioritize user privacy and employ strong data encryption measures to secure personal information. Our platform adheres to strict data protection regulations.',
+  },
+];
+
+
+export default faqFieldsObject;
