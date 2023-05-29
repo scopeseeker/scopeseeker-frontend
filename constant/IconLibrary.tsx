98a9295ab2f322cd2b03a7d15e634f3a1ff40dc3
Abhijeet Raj <abhijeetraj900@gmail.com>
2023-05-29 16:12:28 +0530

    icon: icon added and updated

--- constant/IconLibrary.tsx
+++ constant/IconLibrary.tsx
@@ -216,64 +216,63 @@ export const IconData: IconData = {
     </>
   ),
 
-  // Place icon it's dummy
+  
 
   pages: (
     <>
-      <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-      <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
-      <path d="M9 17h6" />
-      <path d="M9 13h6" />
+      <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0"></path>
+      <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0"></path>
+      <path d="M3 6l0 13"></path>
+      <path d="M12 6l0 13"></path>
+      <path d="M21 6l0 13"></path>
     </>
   ),
   arrowDown: (
     <>
-      <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
-      <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
-      <path d="M12 12l0 .01" />
-      <path d="M3 13a20 20 0 0 0 18 0" />
+      <path d="M12 5l0 14"></path>
+      <path d="M18 13l-6 6"></path>
+      <path d="M6 13l6 6"></path>
     </>
   ),
   gmail: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M4 6l16 0" />
-      <path d="M4 12l16 0" />
-      <path d="M4 18l16 0" />
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"></path>
+      <path d="M3 7l9 6l9 -6"></path>
     </>
   ),
-  arrow: (
+  arrowRight: (
     <>
-      <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"></path>
-      <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1"></path>
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M5 12l14 0"></path>
+      <path d="M13 18l6 -6"></path>
+      <path d="M13 6l6 6"></path>
     </>
   ),
   quotesLeft: (
     <>
-      <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"></path>
+      <path d="M10 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5"></path>
+      <path d="M19 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5"></path>
     </>
   ),
   quotesRight: (
     <>
-      <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
-      <path d="M8 11l0 5"></path>
-      <path d="M8 8l0 .01"></path>
-      <path d="M12 16l0 -5"></path>
-      <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+      <path d="M10 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5"></path>
+      <path d="M19 11h-4a1 1 0 0 1 -1 -1v-3a1 1 0 0 1 1 -1h3a1 1 0 0 1 1 1v6c0 2.667 -1.333 4.333 -4 5"></path>
     </>
   ),
   user: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
-      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-      <path d="M16.5 7.5l0 .01"></path>
+      <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
+      <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
     </>
   ),
   case: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z"></path>
+      <path d="M3 7m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+      <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2"></path>
+      <path d="M12 12l0 .01"></path>
+      <path d="M3 13a20 20 0 0 0 18 0"></path>
     </>
   ),
   company: (
@@ -289,101 +288,92 @@ export const IconData: IconData = {
   ),
   darkMode: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-      <path d="M21 21l-6 -6"></path>
+      <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"></path>
     </>
   ),
 
   form: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
-      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-      <path d="M16.5 7.5l0 .01"></path>
+      <path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+      <path d="M7 8h10"></path>
+      <path d="M7 12h10"></path>
+      <path d="M7 16h10"></path>
     </>
   ),
   fresher: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
-      <path d="M16 19h6"></path>
-      <path d="M19 16v6"></path>
-      <path d="M6 21v-2a4 4 0 0 1 4 -4h4"></path>
+      <path d="M6 21v-2a4 4 0 0 1 4 -4h1.5"></path>
+      <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+      <path d="M20.2 20.2l1.8 1.8"></path>
     </>
   ),
   global: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-      <path d="M21 21l-6 -6"></path>
+      <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+      <path d="M3.6 9h16.8"></path>
+      <path d="M3.6 15h16.8"></path>
+      <path d="M11.5 3a17 17 0 0 0 0 18"></path>
+      <path d="M12.5 3a17 17 0 0 1 0 18"></path>
     </>
   ),
   college: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
-      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-      <path d="M16.5 7.5l0 .01"></path>
+      <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6"></path>
+      <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4"></path>
     </>
   ),
   add: (
     <>
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z"></path>
+      <path d="M12 5l0 14"></path>
+      <path d="M5 12l14 0"></path>
     </>
   ),
   github: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
-      <path d="M16 19h6"></path>
-      <path d="M19 16v6"></path>
-      <path d="M6 21v-2a4 4 0 0 1 4 -4h4"></path>
+      <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
     </>
   ),
   check: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-      <path d="M21 21l-6 -6"></path>
+      <path d="M5 12l5 5l10 -10"></path>
     </>
   ),
 
   searchicon: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M4 4m0 4a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z"></path>
-      <path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
-      <path d="M16.5 7.5l0 .01"></path>
+      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+      <path d="M21 21l-6 -6"></path>
     </>
   ),
   save: (
     <>
-      <path d="M9 4h6a2 2 0 0 1 2 2v14l-5 -3l-5 3v-14a2 2 0 0 1 2 -2"></path>
+      <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path>
+      <path d="M7 11l5 5l5 -5"></path>
+      <path d="M12 4l0 12"></path>
     </>
   ),
   share: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0"></path>
-      <path d="M16 19h6"></path>
-      <path d="M19 16v6"></path>
-      <path d="M6 21v-2a4 4 0 0 1 4 -4h4"></path>
+      <path d="M6 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+      <path d="M18 6m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+      <path d="M18 18m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+      <path d="M8.7 10.7l6.6 -3.4"></path>
+      <path d="M8.7 13.3l6.6 3.4"></path>
     </>
   ),
   location: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-      <path d="M21 21l-6 -6"></path>
+      <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
+      <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
     </>
   ),
   time: (
     <>
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-      <path d="M21 21l-6 -6"></path>
+      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+      <path d="M12 12h3.5"></path>
+      <path d="M12 7v5"></path>
     </>
   ),
 };
