71dc181638244a0bccb23a7259a1d52f89763211
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2023-06-08 16:33:19 +0530

    style: make some changes

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,6 +1,14 @@
 import { Layout, MainHeading, MyButton, MyIcon, MyText } from '@/component';
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogCloseButton,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
   Box,
+  Button,
   HStack,
   Switch,
   Tab,
@@ -9,17 +17,23 @@ import {
   TabPanels,
   Tabs,
   VStack,
+  useDisclosure,
 } from '@chakra-ui/react';
 import Head from 'next/head';
+import React, { useRef, useState } from 'react';
 
 export default function Settings() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef(null);
+  const [isVerify, setIsVerify] = useState(true);
+
   return (
     <>
       <Head>
         <title>Settings | Scope Seeker</title>
       </Head>
       <Layout>
-        <VStack h={'100vh'} w={'full'} alignItems={'flex-start'} gap={'20px'}>
+        <VStack w={'full'} alignItems={'flex-start'} gap={'20px'}>
           <MainHeading title="Settings" align={'start'} />
           <Tabs variant="unstyled" w={'full'}>
             <TabList gap={'32px'} marginBottom={'10px'}>
@@ -45,67 +59,97 @@ export default function Settings() {
               </Tab>
             </TabList>
             <TabPanels>
+              {/* Account setting Tab */}
               <TabPanel>
-                <VStack alignItems={'flex-start'} gap={'5px'} w={'60%'}>
+                <VStack alignItems={'flex-start'} gap={'5px'} w={'80%'}>
                   <MyText as="heading" title="Account Setting" />
 
                   <VStack alignItems={'flex-start'} w={'full'} gap={'20px'}>
                     {/* Email */}
-                    <Box
-                      borderRadius={'5px'}
-                      border={'1px solid'}
-                      borderColor={'brand.lightgray'}
-                      px={'10px'}
-                      py={'15px'}
-                      _hover={{ bg: 'brand.aliceblue' }}
-                      w={'full'}
+                    <SettingElemsDivTemplate
+                      label="Email:"
+                      value="lokesh@gmail.com"
+                      button="Change Email"
+                      isVerify={isVerify}
                     >
-                      <HStack position={'relative'}>
-                        <MyText as="title" title="Email:" />
-                        <HStack
-                          justifyContent={'space-between'}
-                          w={'75%'}
-                          position={'absolute'}
-                          right={0}
-                        >
-                          <HStack spacing={0} gap={'4px'}>
-                            <MyText as="span" title="lokesh1234@gmail.com" />
-                            <MyIcon
-                              name="verifyFill"
-                              width="15px"
-                              color="brand.primary"
-                            />
-                          </HStack>
-                          <MyButton title="change Email" />
-                        </HStack>
-                      </HStack>
-                    </Box>
+                      <MyIcon name="verifyFill" color="green" width="18px" />
+                    </SettingElemsDivTemplate>
 
                     {/* Password */}
-                    <SettingElems
+                    <SettingElemsDivTemplate
                       label="Password:"
                       value="**************"
                       button="Change Password"
                     />
 
                     {/* Phone number */}
-                    <SettingElems
+                    <SettingElemsDivTemplate
                       label="Phone No. :"
-                      value="91XXXXXXXX32"
-                      button="change number"
-                    />
+                      value="+91XXXXXXXX32"
+                      button="Change Number"
+                      isVerify={isVerify}
+                    >
+                      <MyIcon name="verifyFill" color="green" width="18px" />
+                    </SettingElemsDivTemplate>
 
                     {/* Delete Account */}
                     <HStack gap={'40px'}>
                       <MyText as="title" title="Delete my account:" />
-                      <MyButton title={'Delete'} variant="outline" h="25px" />
+                      <MyButton
+                        title={'Delete Account'}
+                        variant="outline"
+                        h="30px"
+                        borderColor={'red'}
+                        borderWidth={'1.5px'}
+                        color={'red'}
+                        onClick={onOpen}
+                      />
                     </HStack>
 
-                    <MyButton title="SignOut" />
+                    <AlertDialog
+                      isOpen={isOpen}
+                      leastDestructiveRef={cancelRef}
+                      onClose={onClose}
+                      isCentered
+                    >
+                      <AlertDialogOverlay>
+                        <AlertDialogContent>
+                          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            You want to delete the account?
+                          </AlertDialogHeader>
+
+                          <AlertDialogBody>
+                            Are you sure? You can&aposa;t undo this action afterwards.
+                          </AlertDialogBody>
+
+                          <AlertDialogFooter>
+                            <MyButton
+                              title="Cancel"
+                              ref={cancelRef}
+                              onClick={onClose}
+                              borderRadius="5px"
+                              variant="outline"
+                            />
+                            <MyButton
+                              title="Delete"
+                              colorScheme="red"
+                              onClick={onClose}
+                              borderRadius="5px"
+                              ml={'10px'}
+                              bg={'red'}
+                              _hover={{ bg: 'red.500' }}
+                            />
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialogOverlay>
+                    </AlertDialog>
+
+                    <MyButton title="SignOut" px="50px" />
                   </VStack>
                 </VStack>
               </TabPanel>
 
+              {/* Email setting Tab */}
               <TabPanel>
                 <VStack alignItems={'flex-start'} gap={'5px'}>
                   <MyText as={'heading'} title="Newsletter Preference" />
@@ -122,66 +166,17 @@ export default function Settings() {
                       />
                     </VStack>
 
-                    <Box
-                      borderRadius={'5px'}
-                      border={'1px solid'}
-                      borderColor={'brand.lightgray'}
-                      px={'10px'}
-                      py={'15px'}
-                      _hover={{ bg: 'brand.aliceblue' }}
-                      w={'60%'}
-                    >
-                      <HStack justifyContent={'space-between'}>
-                        <MyText as="title" title="Newsletters" />
-                        <Switch color={'brand.primary'} />
-                      </HStack>
-                    </Box>
+                    <EmailSettingElemsDivTemplate
+                      label="Newsletter"
+                      dets="From here, you can set the preference for receiving our newsletters."
+                    />
 
-                    <Box
-                      borderRadius={'5px'}
-                      border={'1px solid'}
-                      borderColor={'brand.lightgray'}
-                      px={'10px'}
-                      py={'15px'}
-                      _hover={{ bg: 'brand.aliceblue' }}
-                      w={'60%'}
-                    >
-                      <HStack justifyContent={'space-between'}>
-                        <VStack alignItems={'flex-start'}>
-                          <MyText as="title" title="Job Notifications" />
-                          <VStack
-                            spacing={0}
-                            alignItems={'flex-start'}
-                            gap={'2px'}
-                          >
-                            <MyText
-                              as="p"
-                              title="Here you will find a job updates on email"
-                            />
-                            <MyText
-                              as="span"
-                              title="Stay updated on the latest job opportunities with XYZ Enterprises by subscribing to our email notifications."
-                            />
-                          </VStack>
-                        </VStack>
-                        <Switch color={'brand.primary'} />
-                      </HStack>
-                    </Box>
+                    <EmailSettingElemsDivTemplate
+                      label="Job Notifications"
+                      dets="Stay updated on the latest job opportunities with XYZ Enterprises by subscribing to our email notifications."
+                    />
 
-                    <Box
-                      borderRadius={'5px'}
-                      border={'1px solid'}
-                      borderColor={'brand.lightgray'}
-                      px={'10px'}
-                      py={'15px'}
-                      _hover={{ bg: 'brand.aliceblue' }}
-                      w={'60%'}
-                    >
-                      <HStack justifyContent={'space-between'}>
-                        <MyText as="title" title="Blog" />
-                        <Switch color={'brand.primary'} />
-                      </HStack>
-                    </Box>
+                    <EmailSettingElemsDivTemplate label='Blog' dets='New blog post: "How to Land Your Dream Job'/>
                   </VStack>
                 </VStack>
               </TabPanel>
@@ -193,22 +188,30 @@ export default function Settings() {
   );
 }
 
-interface ISettingElems {
+interface ISettingElemsDivTemplate {
   label: string;
   value: string;
   button: string;
+  children?: React.ReactNode;
+  isVerify?: boolean;
 }
 
-const SettingElems = ({ label, value, button }: ISettingElems) => {
+const SettingElemsDivTemplate = ({
+  label,
+  value,
+  button,
+  children,
+  isVerify,
+}: ISettingElemsDivTemplate) => {
   return (
     <Box
-      borderRadius={'5px'}
+      borderRadius={'12px'}
       border={'1px solid'}
       borderColor={'brand.lightgray'}
-      px={'10px'}
-      py={'15px'}
+      p={'20px'}
       _hover={{ bg: 'brand.aliceblue' }}
       w={'full'}
+      bg={'brand.offwhite'}
     >
       <HStack whiteSpace={'nowrap'} position={'relative'}>
         <MyText as="title" title={label} />
@@ -218,10 +221,49 @@ const SettingElems = ({ label, value, button }: ISettingElems) => {
           position={'absolute'}
           w={'75%'}
         >
-          <MyText as="span" title={value} />
+          <HStack spacing={0} gap={'4px'}>
+            <MyText as="span" title={value} />
+            {children}
+          </HStack>
 
-          <MyButton title={button} />
+          <HStack>
+            {isVerify && (
+              <MyButton title={'Verify'} variant={'outline'} h="30px" />
+            )}
+            <MyButton title={button} h="30px" />
+          </HStack>
         </HStack>
+      </HStack>
+    </Box>
+  );
+};
+
+interface IEmailSettingElemsDivTemplate {
+  label: string;
+  dets: string;
+}
+
+const EmailSettingElemsDivTemplate = ({
+  label,
+  dets,
+}: IEmailSettingElemsDivTemplate) => {
+  return (
+    <Box
+      borderRadius={'5px'}
+      border={'1px solid'}
+      borderColor={'brand.lightgray'}
+      p={'20px'}
+      _hover={{ bg: 'brand.aliceblue' }}
+      w={'60%'}
+    >
+      <HStack justifyContent={'space-between'}>
+        <VStack alignItems={'flex-start'}>
+          <MyText as="title" title={label} />
+          <VStack spacing={0} alignItems={'flex-start'} gap={'2px'}>
+            <MyText as="span" title={dets} />
+          </VStack>
+        </VStack>
+        <Switch color={'brand.primary'} />
       </HStack>
     </Box>
   );
