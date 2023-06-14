c189b37f3cd0e0aee4d7de45ce6688e6f40cca6f
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-06-15 02:09:24 +0530

    style: fromdata state working

--- section-components/edit-profile/BasicInformationSection.tsx
+++ section-components/edit-profile/BasicInformationSection.tsx
@@ -72,186 +72,197 @@ const BasicInformationSection = () => {
 
   return (
     <FormControl isRequired>
-      <SectionTempalate>
-        <MyText as="title" title="Basic Information" />
+      <form>
+        <SectionTempalate>
+          <MyText as="title" title="Basic Information" />
 
-        <VStack w={'full'} alignItems={'flex-start'} gap={'22px'}>
-          <MyInput
-            labelTitle="Full Name"
-            type="text"
-            name="fullName"
-            placeholder="Abhishek Kumar"
-            leftElement={<MyIcon name="user" />}
-            onChange={handleInputChange}
-          />
-
-          <HStack
-            w={'full'}
-            flexDir={{ base: 'column', md: 'row' }}
-            spacing={0}
-            gap={'22px'}
-          >
+          <VStack w={'full'} alignItems={'flex-start'} gap={'22px'}>
             <MyInput
-              labelTitle="Email"
+              labelTitle="Full Name"
               type="text"
-              name="email"
-              placeholder="abhishek123@gmail.com"
-              leftElement={<MyIcon name="email" />}
+              name="fullName"
+              placeholder="Abhishek Kumar"
+              leftElement={<MyIcon name="user" />}
               onChange={handleInputChange}
             />
 
-            <MyInput
-              labelTitle="Phone"
-              type="number"
-              name="phone"
-              placeholder="+91XXXXXXXX23"
-              leftElement={<MyIcon name="phone" />}
-              onChange={handleInputChange}
-            />
-          </HStack>
-
-          <MyInput
-            labelTitle="Organization/ College"
-            type="text"
-            name="organisation"
-            placeholder="Sagar Institute of Research Technology and Science (SIRTS), Bhopal"
-            leftElement={<MyIcon name="college" />}
-            onChange={handleInputChange}
-          />
-
-          <HStack
-            w={'full'}
-            flexDir={{ base: 'column', md: 'row' }}
-            spacing={0}
-            gap={'22px'}
-          >
-            <MyInput
-              labelTitle="Country"
-              type="text"
-              placeholder="India"
-              name="country"
-              leftElement={<MyIcon name="global" />}
-              onChange={handleInputChange}
-            />
-            <MyInput
-              labelTitle="City"
-              type="text"
-              placeholder="Bhopal"
-              name="city"
-              leftElement={<MyIcon name="company" />}
-              onChange={handleInputChange}
-            />
-          </HStack>
-
-          <VStack alignItems={'flex-start'}>
-            <MyText as="span" title="Gender" />
-            <RadioButton
-              name="gender"
-              options={['Male', 'Female', 'Other', 'Prefer not to say']}
-              onChange={handleInputChange}
-            />
-          </VStack>
-
-          {/* For User Type */}
-          <VStack alignItems={'flex-start'}>
-            <MyText as="span" title="User Type" />
-            <RadioButton
-              name="userType"
-              options={['College Student', 'Professional', 'Fresher']}
-              onChange={handleInputChange}
-            />
-          </VStack>
-
-          {/* For Domain Selection */}
-          <VStack alignItems={'flex-start'}>
-            <MyText as="span" title="Domain" />
-            <RadioButton
-              name="domain"
-              options={['Tech', 'Non-Tech']}
-              onChange={handleInputChange}
-            />
-          </VStack>
-
-          {/* For Course Selection */}
-          <VStack alignItems={'flex-start'}>
-            <MyText as="span" title="Course" />
-            <RadioButton
-              name="course"
-              options={['B.Tech', 'M.Tech', 'BCA', 'MCA', 'Other']}
-              onChange={handleInputChange}
-            />
-          </VStack>
-
-          {/* For Course Specialization */}
-          <VStack alignItems={'flex-start'}>
-            <MyText as="span" title="Course Specialization" />
-            <Select
-              placeholder="Computer Science"
-              fontSize={'13px'}
-              onChange={handleInputChange}
-              name="courseSpecialization"
+            <HStack
+              w={'full'}
+              flexDir={{ base: 'column', md: 'row' }}
+              spacing={0}
+              gap={'22px'}
             >
-              {[
-                'Aerospace',
-                'Automotive',
-                'Biotechnology',
-                'Chemical',
-                'Civil',
-                'Electrical',
-                'Electronics and Communication',
-                'Information Technology',
-                'Instrument',
-                'Other',
-                'Not Applicable',
-              ].map((value, key) => {
-                return (
-                  <option value={value} key={key}>
-                    {value}
-                  </option>
-                );
-              })}
-            </Select>
-          </VStack>
+              <MyInput
+                labelTitle="Email"
+                type="email"
+                name="email"
+                placeholder="abhishek123@gmail.com"
+                leftElement={<MyIcon name="email" />}
+                onChange={handleInputChange}
+              />
 
-          {/* For Year of Graduation */}
-          <VStack alignItems={'flex-start'}>
-            <MyText as="span" title="Year Of Graduation" />
-            <Select
-              placeholder="2016"
-              fontSize={'13px'}
-              name="yearOfGraduation"
+              <MyInput
+                labelTitle="Phone"
+                type="number"
+                name="phone"
+                placeholder="+91XXXXXXXX23"
+                leftElement={<MyIcon name="phone" />}
+                onChange={handleInputChange}
+              />
+            </HStack>
+
+            <MyInput
+              labelTitle="Organization/ College"
+              type="text"
+              name="organisation"
+              placeholder="Sagar Institute of Research Technology and Science (SIRTS), Bhopal"
+              leftElement={<MyIcon name="college" />}
               onChange={handleInputChange}
+            />
+
+            <HStack
+              w={'full'}
+              flexDir={{ base: 'column', md: 'row' }}
+              spacing={0}
+              gap={'22px'}
             >
-              {['2017', '2018', '2019', '2020', '2021', '2022'].map(
-                (value, key) => {
+              <MyInput
+                labelTitle="Country"
+                type="text"
+                placeholder="India"
+                name="country"
+                leftElement={<MyIcon name="global" />}
+                onChange={handleInputChange}
+              />
+              <MyInput
+                labelTitle="City"
+                type="text"
+                placeholder="Bhopal"
+                name="city"
+                leftElement={<MyIcon name="company" />}
+                onChange={handleInputChange}
+              />
+            </HStack>
+
+            <VStack alignItems={'flex-start'}>
+              <MyText as="span" title="Gender" />
+              <RadioButton
+                name="gender"
+                options={['Male', 'Female', 'Other', 'Prefer not to say']}
+                onChange={handleInputChange}
+              />
+            </VStack>
+
+            {/* For User Type */}
+            <VStack alignItems={'flex-start'}>
+              <MyText as="span" title="User Type" />
+              <RadioButton
+                name="userType"
+                options={['College Student', 'Professional', 'Fresher']}
+                onChange={handleInputChange}
+              />
+            </VStack>
+
+            {/* For Domain Selection */}
+            <VStack alignItems={'flex-start'}>
+              <MyText as="span" title="Domain" />
+              <RadioButton
+                name="domain"
+                options={['Tech', 'Non-Tech']}
+                onChange={handleInputChange}
+              />
+            </VStack>
+
+            {/* For Course Selection */}
+            <VStack alignItems={'flex-start'}>
+              <MyText as="span" title="Course" />
+              <RadioButton
+                name="course"
+                options={['B.Tech', 'M.Tech', 'BCA', 'MCA', 'Other']}
+                onChange={handleInputChange}
+              />
+            </VStack>
+
+            {/* For Course Specialization */}
+            <VStack alignItems={'flex-start'}>
+              <MyText as="span" title="Course Specialization" />
+              <Select
+                placeholder="Computer Science"
+                fontSize={'13px'}
+                onChange={handleInputChange}
+                name="courseSpecialization"
+              >
+                {[
+                  'Aerospace',
+                  'Automotive',
+                  'Biotechnology',
+                  'Chemical',
+                  'Civil',
+                  'Electrical',
+                  'Electronics and Communication',
+                  'Information Technology',
+                  'Instrument',
+                  'Other',
+                  'Not Applicable',
+                ].map((value, key) => {
                   return (
                     <option value={value} key={key}>
                       {value}
                     </option>
                   );
-                }
-              )}
-            </Select>
-          </VStack>
+                })}
+              </Select>
+            </VStack>
 
-          {/* For Summary */}
-          <VStack alignItems={'flex-start'} w={'full'}>
-            <MyText as="span" title="Summary" />
-            <Textarea
-              placeholder="Write about yourself..."
-              resize={'none'}
-              onChange={handleInputChange}
-              name="summary"
-            />
+            {/* For Year of Graduation */}
+            <VStack alignItems={'flex-start'}>
+              <MyText as="span" title="Year Of Graduation" />
+              <Select
+                placeholder="2016"
+                fontSize={'13px'}
+                name="yearOfGraduation"
+                onChange={handleInputChange}
+              >
+                {['2017', '2018', '2019', '2020', '2021', '2022'].map(
+                  (value, key) => {
+                    return (
+                      <option value={value} key={key}>
+                        {value}
+                      </option>
+                    );
+                  }
+                )}
+              </Select>
+            </VStack>
+
+            {/* For Summary */}
+            <VStack alignItems={'flex-start'} w={'full'}>
+              <MyText as="span" title="Summary" />
+              <Textarea
+                placeholder="Write about yourself..."
+                resize={'none'}
+                onChange={handleInputChange}
+                name="summary"
+              />
+            </VStack>
+            <HStack>
+              <MyButton
+                type={'submit'}
+                title="Save"
+                px={'30px'}
+                onClick={handleSubmit}
+              />
+              <MyButton
+                type={'reset'}
+                title="Cancel"
+                px={'30px'}
+                variant="outline"
+                onClick={handleSubmit}
+              />
+            </HStack>
           </VStack>
-          <MyButton
-            type={'submit'}
-            title="Submit"
-            px={'30px'}
-            onClick={handleSubmit}
-          />
-        </VStack>
-      </SectionTempalate>
+        </SectionTempalate>
+      </form>
     </FormControl>
   );
 };
