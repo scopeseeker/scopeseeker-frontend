df7f68e1c951ac9f57bb266913be545b27547796
lokeshdhakar97 <lokeshdhakar6633@gmail.com>
2023-07-15 14:32:31 +0530

    fix: avatar menu navbar box close issue fixed

---
+++ src/constant/linksObject.ts
@@ -0,0 +1,27 @@
+export const avatarMenuLinks = [
+  {
+    title: 'Dashboard',
+    link: '/dashboard',
+    icon: 'dashboard',
+  },
+  {
+    title: 'My Profile',
+    link: '/my-profile',
+    icon: 'profile',
+  },
+  {
+    title: 'Settings',
+    link: '/settings',
+    icon: 'settings',
+  },
+  {
+    title: 'Report an issue',
+    link: '/contact-us',
+    icon: 'questionMark',
+  },
+  {
+    title: 'Logout',
+    link: '/logout',
+    icon: 'logout',
+  },
+];
