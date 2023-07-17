746254685f6d97c9cdb709619f63598184be5d02
abhishekkumar011 <abhishekkumar0336165@gmail.com>
2023-07-17 15:47:13 +0530

    ref: refactor some code in testimonials

--- src/section-components/first-page-sections/Testimonials.tsx
+++ src/section-components/first-page-sections/Testimonials.tsx
@@ -1,6 +1,6 @@
 import { Layout, MainHeading, MyIcon, MyImage, MyText } from '@/component';
 import truncatedParagraph from '@/helpers/validator';
-import { Center, Flex, Grid, Text, VStack } from '@chakra-ui/react';
+import { Center, Flex, Grid, HStack, Text, VStack } from '@chakra-ui/react';
 const Testimonials = () => {
   return (
     <VStack bg={'brand.aliceblue'} py={'42px'}>
@@ -9,188 +9,166 @@ const Testimonials = () => {
           title={'Join Our Thriving Community'}
           subTitle={'See How Our Job Website Has Changed Lives'}
         />
-        <Flex
-          py={'10px'}
-          overflow={'auto'}
+        <HStack
           w={'full'}
-          css={{
-            '&::-webkit-scrollbar': {
-              display: 'none',
-            },
-          }}
+          mt={'100px'}
+          gap={{ base: '70px', sm: '20px' }}
         >
-          <Flex
+          <Grid
+            templateColumns={{
+              base: 'repeat(1, 1fr)',
+              sm: 'repeat(2, 1fr)',
+              md: 'repeat(2, 1fr)',
+              lg: 'repeat(3, 1fr)',
+            }}
+            rowGap={'60px'}
+            columnGap={'20px'}
             w={'full'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            flexShrink={0}
-            pt={'100px'}
-            gap={'24px'}
-            // px={'20px'}
           >
-            <Grid
-              templateColumns={{
-                base: 'repeat(1, 1fr)',
-                sm: 'repeat(2, 1fr)',
-                md: 'repeat(2, 1fr)',
-                lg: 'repeat(3, 1fr)',
-              }}
-              rowGap={'60px'}
-              columnGap={'20px'}
-              w={'full'}
+            <VStack
+              bg={'brand.white'}
+              boxShadow={'0 0px 30px rgba(0, 0, 0, 0.1)'}
+              borderRadius={'10px'}
             >
-              <VStack
-                w="100%"
-                h={{ base: 'fit-content', md: '248px' }}
-                bg={'brand.white'}
-                boxShadow={'0 0px 30px rgba(0, 0, 0, 0.1)'}
-                flexShrink={0}
-                borderRadius={8}
-                justifyContent={'flex-start'}
-                alignItems={'center'}
-                px={'28px'}
-                pb={'20px'}
-                display={{
-                  base: 'flex',
-                  sm: 'none',
-                  lg: 'flex',
-                }}
+              <Center
+                border={'3px solid white'}
+                h={'78px'}
+                w={'78px'}
+                mt={'-34px'}
+                borderRadius={'full'}
+                overflow={'hidden'}
               >
+                <MyImage
+                  src={'/assets/images/p12.jpg'}
+                  alt={'secope seeker logo'}
+                  width={100}
+                  height={100}
+                  style={{ borderRadius: '50%' }}
+                />
+              </Center>
+
+              <VStack px={'25px'} pt={'10px'} pb={'30px'} textAlign={'center'}>
+                <VStack lineHeight={1}>
+                  <MyText as="heading" title="Lokesh Dhakar" />
+                  <MyText as="span" title="Full Stack Developer" />
+                </VStack>
                 <Center
-                  border={'3px solid white'}
-                  h={'78px'}
-                  w={'78px'}
-                  mt={'-34px'}
-                  borderRadius={'full'}
-                  overflow={'hidden'}
+                  opacity={'0.7'}
+                  justifyContent={'flex-start'}
+                  width={'full'}
                 >
-                  <MyImage
-                    src={'/assets/images/p12.jpg'}
-                    alt={'secope seeker logo'}
-                    width={100}
-                    height={100}
-                    style={{ borderRadius: '50%' }}
-                  />
+                  <MyIcon name={'quotesLeft'} width={'full'} />
                 </Center>
-                <Flex direction={'column'} align={'center'}>
-                  <MyText as={'heading'} title="Lokesh Dhakar" />
-                  <MyText as={'span'} title="Full Stack Developer" />
-                  <Text as={'span'} fontSize={'sm'} textAlign={'center'}>
-                    <Center w={8} h={8} opacity={'0.7'} py={'10px'}>
-                      <MyIcon name={'quotesLeft'} width={'full'} />
-                    </Center>
-                    <MyText
-                      as={'span'}
-                      title={truncatedParagraph(
-                        `I’ve got your back. These sixty review request text templates
+                <MyText
+                  as="span"
+                  title={truncatedParagraph(
+                    `I’ve got your back. These sixty review request text templates
                     are designed to get you up and running fast. Of course, you
-                    may want to come up doing so mch imaportant thhisngs.`,
-                        70,
-                      )}
-                      color="brand.darkgray"
-                    />
-                  </Text>
-                </Flex>
+                    may want to come up doing so mch imaportant things.`,
+                    70,
+                  )}
+                  color="brand.darkgray"
+                />
               </VStack>
-              <VStack
-                w="100%"
-                h={{ base: 'fit-content', md: '248px' }}
-                bg={'brand.white'}
-                boxShadow={'0 0px 30px rgba(0, 0, 0, 0.1)'}
-                flexShrink={0}
-                borderRadius={8}
-                justifyContent={'flex-start'}
-                alignItems={'center'}
-                px={'28px'}
-                pb={'20px'}
+            </VStack>
+
+            {/* 2nd box */}
+            <VStack
+              bg={'brand.white'}
+              boxShadow={'0 0px 30px rgba(0, 0, 0, 0.1)'}
+              borderRadius={'10px'}
+            >
+              <Center
+                border={'3px solid white'}
+                h={'78px'}
+                w={'78px'}
+                mt={'-34px'}
+                borderRadius={'full'}
+                overflow={'hidden'}
               >
+                <MyImage
+                  src={'/assets/images/p12.jpg'}
+                  alt={'secope seeker logo'}
+                  width={100}
+                  height={100}
+                  style={{ borderRadius: '50%' }}
+                />
+              </Center>
+
+              <VStack px={'25px'} pt={'10px'} pb={'30px'} textAlign={'center'}>
+                <VStack lineHeight={1}>
+                  <MyText as="heading" title="Lokesh Dhakar" />
+                  <MyText as="span" title="Full Stack Developer" />
+                </VStack>
                 <Center
-                  border={'3px solid white'}
-                  h={'78px'}
-                  w={'78px'}
-                  mt={'-34px'}
-                  borderRadius={'full'}
-                  overflow={'hidden'}
+                  opacity={'0.7'}
+                  justifyContent={'flex-start'}
+                  width={'full'}
                 >
-                  <MyImage
-                    src={'/assets/images/p12.jpg'}
-                    alt={'secope seeker logo'}
-                    width={100}
-                    height={100}
-                    style={{ borderRadius: '50%' }}
-                  />
+                  <MyIcon name={'quotesLeft'} width={'full'} />
                 </Center>
-                <Flex direction={'column'} align={'center'}>
-                  <MyText as={'heading'} title="Lokesh Dhakar" />
-                  <MyText as={'span'} title="Full Stack Developer" />
-                  <Text as={'span'} fontSize={'sm'} textAlign={'center'}>
-                    <Center w={8} h={8} opacity={'0.7'} py={'10px'}>
-                      <MyIcon name={'quotesLeft'} width={'full'} />
-                    </Center>
-                    <MyText
-                      as={'span'}
-                      title={truncatedParagraph(
-                        `I’ve got your back. These sixty review request text templates
+                <MyText
+                  as="span"
+                  title={truncatedParagraph(
+                    `I’ve got your back. These sixty review request text templates
                     are designed to get you up and running fast. Of course, you
-                    may want to come up doing so mch imaportant thhisngs.`,
-                        70,
-                      )}
-                      color="brand.darkgray"
-                    />
-                  </Text>
-                </Flex>
+                    may want to come up doing so mch imaportant things.`,
+                    70,
+                  )}
+                  color="brand.darkgray"
+                />
               </VStack>
-              <VStack
-                w="100%"
-                h={{ base: 'fit-content', md: '248px' }}
-                bg={'brand.white'}
-                boxShadow={'0 0px 30px rgba(0, 0, 0, 0.1)'}
-                flexShrink={0}
-                borderRadius={8}
-                justifyContent={'flex-start'}
-                alignItems={'center'}
-                px={'28px'}
-                pb={'20px'}
+            </VStack>
+
+            {/* 3rd Box */}
+            <VStack
+              bg={'brand.white'}
+              boxShadow={'0 0px 30px rgba(0, 0, 0, 0.1)'}
+              borderRadius={'10px'}
+            >
+              <Center
+                border={'3px solid white'}
+                h={'78px'}
+                w={'78px'}
+                mt={'-34px'}
+                borderRadius={'full'}
+                overflow={'hidden'}
               >
+                <MyImage
+                  src={'/assets/images/p12.jpg'}
+                  alt={'secope seeker logo'}
+                  width={100}
+                  height={100}
+                  style={{ borderRadius: '50%' }}
+                />
+              </Center>
+
+              <VStack px={'25px'} pt={'10px'} pb={'30px'} textAlign={'center'}>
+                <VStack lineHeight={1}>
+                  <MyText as="heading" title="Lokesh Dhakar" />
+                  <MyText as="span" title="Full Stack Developer" />
+                </VStack>
                 <Center
-                  border={'3px solid white'}
-                  h={'78px'}
-                  w={'78px'}
-                  mt={'-34px'}
-                  borderRadius={'full'}
-                  overflow={'hidden'}
+                  opacity={'0.7'}
+                  justifyContent={'flex-start'}
+                  width={'full'}
                 >
-                  <MyImage
-                    src={'/assets/images/p12.jpg'}
-                    alt={'secope seeker logo'}
-                    width={100}
-                    height={100}
-                    style={{ borderRadius: '50%' }}
-                  />
+                  <MyIcon name={'quotesLeft'} width={'full'} />
                 </Center>
-                <Flex direction={'column'} align={'center'}>
-                  <MyText as={'heading'} title="Lokesh Dhakar" />
-                  <MyText as={'span'} title="Full Stack Developer" />
-                  <Text as={'span'} fontSize={'sm'} textAlign={'center'}>
-                    <Center w={8} h={8} opacity={'0.7'} py={'10px'}>
-                      <MyIcon name={'quotesLeft'} width={'full'} />
-                    </Center>
-                    <MyText
-                      as={'span'}
-                      title={truncatedParagraph(
-                        `I’ve got your back. These sixty review request text templates
+                <MyText
+                  as="span"
+                  title={truncatedParagraph(
+                    `I’ve got your back. These sixty review request text templates
                     are designed to get you up and running fast. Of course, you
-                    may want to come up doing so mch imaportant thhisngs.`,
-                        70,
-                      )}
-                      color="brand.darkgray"
-                    />
-                  </Text>
-                </Flex>
+                    may want to come up doing so mch imaportant things.`,
+                    70,
+                  )}
+                  color="brand.darkgray"
+                />
               </VStack>
-            </Grid>
-          </Flex>
-        </Flex>
+            </VStack>
+          </Grid>
+        </HStack>
       </Layout>
     </VStack>
   );
